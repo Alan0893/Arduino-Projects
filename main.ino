28f09265465c1c1eee3377d84e4bd3a0ff393dc3
Alan <89307499+Alan0893@users.noreply.github.com>
2022-01-27 16:00:26 -0500

    Update main.ino

--- main.ino
+++ main.ino
@@ -42,7 +42,261 @@ void setup()
   pinMode(b4, INPUT);
   pinMode(b5, INPUT);
   pinMode(b6, INPUT);
+  pinMode(b7, INPUT);//LCD
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12,11,5,4,3,2);
+
+//LEDs and Buttons and buzzers
+int led1;
+int b1, b2, b3, b4, b5, b6, b7, b8;
+int buzzer;
+
+//Globals
+long future;
+double ans;
+int displayNum;
+int currNum;
+int operation;
+int firstNum;
+int secondNum;
+int posCursor;
+boolean gotAns;
+
+//SETUP 
+void setup()
+{
+  Serial.begin(9600);
+
+  //LEDs
+  led1 = 8;
+  pinMode(led1, OUTPUT);
+
+  //BUTTONS
+  b1 = A0;
+  b2 = A1;
+  b3 = A2;
+  b4 = A3; 
+  b5 = A4;
+  b6 = A5;
+  b7 = 7;
+  b8 = 6;
+  pinMode(b1, INPUT);
+  pinMode(b2, INPUT);
+  pinMode(b3, INPUT);
+  pinMode(b4, INPUT);
+  pinMode(b5, INPUT);
+  pinMode(b6, INPUT);
   pinMode(b7, INPUT);
+  pinMode(b8, INPUT);
+
+  //BUZZER
+  buzzer = 9;
+  pinMode(buzzer,OUTPUT);
+
+  //Setting up the LCD number of columns and rows
+  lcd.begin(16, 2);
+
+  //Instantiating Global Variables
+  future = 0;     //For Button Pressing
+  ans = 0;        //Final Answer
+  displayNum = 5; //The current single number being displayed
+  currNum = 0;    //The current whole number being displayed
+  operation = 0;  //The operation being called (+, -, x, /)
+  firstNum = 0;   //The first number 
+  secondNum = 0;  //The second number after the operation
+  posCursor = 0;  //The position of the cursor on the LCD
+  gotAns = false; //Checks if answer was obtained
+}
+
+void loop() {
+  //checkButtons();
+  onClick();
+}
+void onClick(){
+  //The states of the buttons
+  int b1state = digitalRead(b1);
+  int b2state = digitalRead(b2);
+  int b3state = digitalRead(b3);
+  int b4state = digitalRead(b4);
+  int b5state = digitalRead(b5);
+  int b6state = digitalRead(b6);
+  int b7state = digitalRead(b7);
+  int b8state = digitalRead(b8);
+
+  //Time for future (button pressing)
+  long time = millis();
+  //int secs = time/1000;
+  
+  //Setting the cursor of the LCD
+  if(!gotAns) {
+    lcd.setCursor(posCursor,0);
+    blinkNum(displayNum);
+  }
+  else if(gotAns) {
+    lcd.print(" ");
+  }
+  
+  if(b1state == 1 && time >= future){ //BUTTON 1 (<--)
+    if(displayNum != 0){  //While the current displayed number isn't 0
+      displayNum--;       //Subtract 1 : 5 --> 4 --> 3 ...
+    }
+    future = time + 250;  //Prevents user from pressing button for 1/4 sec
+    
+    lcd.setCursor(posCursor,0); //Setting the cursor of the LCD
+    lcd.print(displayNum);      //Displaying the number on the LCD 
+  }
+  else if(b2state == 1 && time >= future){  //BUTTON 2 (SELECT)
+    future = time + 250;  //Prevents user from pressing button for 1/4 sec
+
+    lcd.setCursor(posCursor,0); //Setting the cursor of the LCD
+    lcd.print(displayNum);    //Displaying the number on the LCD
+
+    currNum = currNum * 10 + displayNum;  //Getting the current number
+    displayNum = 5; //Resetting the displayed number to 5 for next number
+    posCursor++;  //Adding a column to the cursor position
+  }
+  else if(b3state == 1 && time >= future){ //BUTTON 3 (-->)
+    if(displayNum != 9){  //While the current displayed number isn't 9
+      displayNum++;       //Add 1 5 --> 6 --> 7 ...
+    }
+    future = time + 250;  //Prevents user from pressing button for 1/4 sec
+    
+    lcd.setCursor(posCursor,0); //Setting the cursor of the LCD
+    lcd.print(displayNum);      //Displaying the number on the LCD
+  }
+  else if(b4state == 1 && time >= future){  //BUTTON 4 (+)
+    lcd.setCursor(posCursor,0); //Setting the cursor of the LCD
+    lcd.print("+");   //Printing the plus sign
+    posCursor++;  //Adding a column to the cursor position
+    
+    firstNum = currNum; //Setting the first number to the current number obtained
+    currNum = 0;  //Resetting currNum to 0 for second numer
+    operation = 1;  //Making the operation to 1 for addition
+    future = time + 250;  //Prevents user from pressing button for 1/4 sec
+  }
+  else if(b5state == 1 && time >= future){  //BUTTON 5 (-)
+    lcd.setCursor(posCursor,0);
+    lcd.print("-");
+    posCursor++;
+    
+    firstNum = currNum;
+    currNum = 0;
+    operation = 2;
+    future = time + 250;
+  }
+  else if(b6state == 1 && time >= future){  //BUTTON 6 (x)
+    lcd.setCursor(posCursor,0);
+    lcd.print("x");
+    posCursor++;
+
+    firstNum = currNum;
+    currNum = 0;
+    operation = 3;
+    future = time + 250;
+  }
+  else if(b7state == 1 && time >= future){  //BUTTON 7 (/)
+    lcd.setCursor(posCursor,0);
+    lcd.print("/");
+    posCursor++;
+    
+    firstNum = currNum;
+    currNum = 0;
+    operation = 4;
+    future = time + 250;
+  }
+  else if(b8state == 1 && time >= future){  //BUTTON 8 (=)
+    secondNum = currNum;
+    currNum = 0;
+    if(operation == 0){
+      ans = firstNum;
+    }
+    else if(operation == 1){
+      ans = firstNum + secondNum;
+    }
+    else if(operation == 2){
+      ans = firstNum - secondNum;
+    }
+    else if(operation == 3){
+      ans = firstNum * secondNum;
+    }
+    else if(operation == 4){
+      ans = firstNum / secondNum;
+    }
+    lcd.setCursor(0,1);
+    lcd.print(ans);
+    gotAns = true;
+
+    posCursor = 0;
+    operation = 0;
+    future = time + 250;
+  }
+  else{
+    noTone(buzzer);
+  }
+}
+
+//Blinking the current number on the LCD
+void blinkNum(int num) {
+  long time = millis();
+  long sec = time/500;
+  
+  if(sec%2 == 0)
+    lcd.print(num);
+  else
+    lcd.print(" ");
+}
+
+//Blinks the LEDs
+void blinkLED(int led) {
+  long time = millis();
+  long sec = time/500;
+
+  if(sec%2 == 0)
+    digitalWrite(led, HIGH);
+  else
+    digitalWrite(led, LOW);
+}
+
+//Plays a tone on the buzzer every 1/2 second
+void playTone(int buzzer, double tone) {
+  long time = millis();
+  long sec = time/500;
+
+  if(sec%2 == 0)
+    tone(buzzer, tone);
+  else
+    noTone(buzzer);
+}
+
+//Checking the Button state
+void checkButtons(){
+  int b1state = digitalRead(b1);
+  int b2state = digitalRead(b2);
+  int b3state = digitalRead(b3);
+  int b4state = digitalRead(b4);
+  int b5state = digitalRead(b5);
+  int b6state = digitalRead(b6);
+  int b7state = digitalRead(b7);
+  int b8state = digitalRead(b8);
+
+  Serial.print("b1 = ");
+  Serial.println(b1state);
+  Serial.print("b2 = ");
+  Serial.println(b2state);
+  Serial.print("b3 = ");
+  Serial.println(b3state);
+  Serial.print("b4 = ");
+  Serial.println(b4state);
+  Serial.print("b5 = ");
+  Serial.println(b5state);
+  Serial.print("b6 = ");
+  Serial.println(b6state);
+  Serial.print("b7 = ");
+  Serial.println(b7state);
+  Serial.print("b8 = ");
+  Serial.println(b8state);
+}
+
   pinMode(b8, INPUT);
 
   //BUZZER
